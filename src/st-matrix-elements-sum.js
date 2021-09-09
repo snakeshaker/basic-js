d644eafe1c98cd372cd35f5a9db3f17e74c67b01
snakeshaker <brabus870@gmail.com>
2021-09-09 16:33:05 +0600

    feat: add st-matrix-elements-sum.js sollution

--- src/st-matrix-elements-sum.js
+++ src/st-matrix-elements-sum.js
@@ -16,7 +16,17 @@ import { NotImplementedError } from '../extensions/index.js';
  *
  * The result should be 9
  */
-export default function getMatrixElementsSum(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function getMatrixElementsSum(matrix) {
+  let result = 0;
+  for(let i = 0; i < matrix[0].length; i++) {
+    result += matrix[0][i];
+  }
+  for(let i = 1; i < matrix.length; i++) {
+    for(let j = 0; j < matrix[i].length; j++) {
+      if(matrix[i - 1][j] != 0) {
+        result += matrix[i][j];
+      }
+    }
+  }
+  return result;
 }
