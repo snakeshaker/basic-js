453c472be2a6362fcfa13f9ea353a121d1fabfae
snakeshaker <brabus870@gmail.com>
2021-09-10 22:16:11 +0600

    feat: add vigenere-cipher.js solution

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,12 +20,51 @@ import { NotImplementedError } from '../extensions/index.js';
  * 
  */
 export default class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(direct = true) {
+    this.direct = direct
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  encrypt(message, key) {
+    if (!message || !key) {
+      throw new Error('Incorrect arguments!');
+    }
+    let result = [];
+    message = message.toUpperCase();
+    key = key.toUpperCase();
+    const alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+    for (let i = 0, j = 0; i < message.length; i++) {
+      if (alphabet.indexOf(message[i]) == -1) {
+        result.push(message[i]);
+      } else {
+        let count = (alphabet.indexOf(message[i]) + alphabet.indexOf(key[j])) % 26;
+        result.push(alphabet[count]);
+        if ((j + 1) == key.length) {
+          j = 0;
+        } else j += 1;
+      }
+    }
+    return (!this.direct) ? result.reverse().join('') : result.join('');
+  }
+
+  decrypt(encryptedMessage, key) {
+    if (!encryptedMessage || !key) {
+      throw new Error('Incorrect arguments!');
+    }
+    let result = [];
+    encryptedMessage = encryptedMessage.toUpperCase();
+    key = key.toUpperCase();
+    const alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+    for (let i = 0, j = 0; i < encryptedMessage.length; i++) {
+      if (alphabet.indexOf(encryptedMessage[i]) == -1) {
+        result.push(encryptedMessage[i]);
+      } else {
+        let count = (alphabet.indexOf(encryptedMessage[i]) - alphabet.indexOf(key[j]) + 26) % 26;
+        result.push(alphabet[count]);
+        if ((j + 1) == key.length) {
+          j = 0;
+        } else j += 1;
+      }
+    }
+    return (!this.direct) ? result.reverse().join('') : result.join('');
   }
 }
